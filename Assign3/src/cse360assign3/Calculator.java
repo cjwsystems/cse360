2aa8ae908f1594dc150a148546952a4db588c4dc
Chris Ward <caesartopher@gmail.com>
2016-02-21 14:47:47 -0700

    part2

---
+++ Assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,67 @@
+
+package cse360assign3;
+/**
+ * Calculator Class for basic arithmetic
+ * @author Chris Ward
+ * @version Feb 21,206
+ * 
+ */
+
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * @param Calculator() default constructor. 
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * @return getTotal returns variable "total"
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Add
+	 * @param value is input number to be added
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtract
+	 * @param value is input number to be subtracted
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Multiply
+	 * @param value is input number to be multiplied
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * Divide
+	 * @param value is input number to be divided
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * getHistory
+	 * @return returns history as string
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
---
+++ Assign3/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,51 @@
+package cse360assign3;
+/**
+ * Calculator JUnit Test Class 
+ * @author Chris Ward
+ * @version Feb 21,206
+ */
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+	
+	@Test
+	public void testCalculator() {
+		Calculator calc = new Calculator();
+		assertNotNull(calc);
+	}
+	
+	@Test
+	public void testGetTotal() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+	
+	
+	@Test
+	public void testSubtract() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testMultiply() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDivide() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHistory() {
+		fail("Not yet implemented");
+	}
+
+
+}
